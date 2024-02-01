bda22bc779f3a96341134dec811b0c741d272241
Andrey Baboshin <andrey.baboshin@gmail.com>
2024-02-01 01:13:17 +0100

    webpack

--- webui/server/src/app.ts
+++ webui/server/src/app.ts
@@ -1,19 +1,31 @@
 import path from "path";
-import express from "express";
+import express, { NextFunction, Request, RequestHandler, Response } from "express";
+import cors from "cors";
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../../client/build')));
+app.use(cors());
+app.use(express.json() as RequestHandler);
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  if (/(.ico|.js|.css|.jpg|.png|.map|.svg)$/i.test(req.path)) {
+    next();
+  } else {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  }
 });
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
