b169f48c7f692c6260ba40f5a044044fb0ba2f4e
Andrey Baboshin <andrey.baboshin@gmail.com>
2024-02-09 20:39:38 +0100

    feat: turnout

---
+++ firmware/src/Turnout.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+#include "BaseDevice.h"
+#include "Switch.h"
+
+class Turnout : public BaseDevice
+{
+private:
+  Switch sw1;
+  Switch sw2;
+
+public:
+  Turnout(std::uint8_t pin1, std::uint8_t pin2);
+  std::shared_ptr<railschema::State> ProcessCommand(std::shared_ptr<railschema::Command> command) override;
+  std::shared_ptr<railschema::Event> Loop();
+};
