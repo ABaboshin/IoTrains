2ca7dea36b758bc9db89a8bef8f988ad7275a901
Andrey Baboshin <ABaboshin@users.noreply.github.com>
2024-02-28 21:18:39 +0100

    feat: VT95 train (#16)

--- firmware/basic_train/src/main.cpp
+++ firmware/basic_train/src/main.cpp
@@ -5,17 +5,20 @@
 
 #include "config.h"
 
+#define STRINGIZE(x) #x
+#define STRINGIZE_VALUE_OF(x) STRINGIZE(x)
+
 ControlUnit cu(wifiNetwork, wifiPassword, mqttServer, mqttClientId, mqttLogin, mqttPassword, 1000 * 5);
 
 void setup()
 {
   Serial.begin(115200);
-  cu.id = "basic train";
+  cu.id = STRINGIZE_VALUE_OF(NAME);
 
   Serial.println("train");
   std::map<std::string, std::vector<unsigned char>> sounds;
   sounds["test"] = std::vector<unsigned char>();
-  std::shared_ptr<BaseDevice> train = std::make_shared<SoundTrain>("basic_train", 1, 2, 3, 4, 0, sounds);
+  std::shared_ptr<BaseDevice> train = std::make_shared<SoundTrain>(STRINGIZE_VALUE_OF(NAME), 1, 2, 3, 4, 0, sounds);
 
   cu.devices.push_back(train);
 
